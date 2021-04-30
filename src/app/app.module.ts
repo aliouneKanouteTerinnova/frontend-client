af615b73ff3fc4f3d8662ae98062865415c79e0d
Abdoulaye NDAO <abdoulaye.ndao@xegit.com>
2021-04-30 11:18:20 +0000

    fix:Sorting the imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateModule } from '@ngx-translate/core';
+
 import { AppRoutingModule } from './app-routing.module';
-import {TranslateModule} from '@ngx-translate/core';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    TranslateModule.forRoot()
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, TranslateModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
