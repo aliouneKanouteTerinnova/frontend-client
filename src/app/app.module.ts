e16058160122c24a34c9d446a56b7fba74ee8fee
Mbaye Jacques DIOP <mbaye.diop@xegit.com>
2021-05-05 13:30:24 +0000

    create header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { TranslateModule } from '@ngx-translate/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, TranslateModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
