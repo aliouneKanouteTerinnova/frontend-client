0e324c68197deba9493dc08ae58674d87041e6b2
Abdoulaye NDAO <abdoulaye.ndao@xegit.com>
2021-05-07 09:55:11 +0000

    refactor: Creation of sharedModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateModule } from '@ngx-translate/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
+import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    TranslateModule.forRoot(),
-    MatButtonModule,
-    MatToolbarModule,
-    MatIconModule,
-  ],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, TranslateModule.forRoot(), SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [CommonModule, MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule],
+})
+export class SharedModule {}
