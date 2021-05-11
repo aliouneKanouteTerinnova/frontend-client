1ec5d67cb7274e21557958b74a8e7dcd65e5bfa8
Mbaye Jacques DIOP <mbaye.diop@xegit.com>
2021-05-11 21:13:25 +0000

    feat: add validation form

---
+++ src/app/pages/stores/store.ts
@@ -0,0 +1,6 @@
+export class Store {
+  name: string;
+  created_at: boolean;
+  created_by: string;
+  store_adress: string;
+}
--- src/app/pages/stores/stores.component.ts
+++ src/app/pages/stores/stores.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Store } from './store';
 
 @Component({
   selector: 'app-stores',
@@ -8,10 +9,32 @@ import { FormGroup } from '@angular/forms';
 })
 export class StoresComponent implements OnInit {
   createStore: FormGroup;
+  submited = false;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.createStore = this.fb.group({
+      name: ['', Validators.required],
+      created_at: ['', Validators.required],
+      created_by: ['', Validators.required],
+      store_adress: ['', Validators.required],
+    });
+  }
 
-  onSubmit() {}
+  get f() {
+    return this.createStore.controls;
+  }
+
+  onSubmit() {
+    this.submited = true;
+    const data = new Store();
+
+    data.name = this.createStore.get('name').value;
+    data.created_at = this.createStore.get('created_at').value;
+    data.created_by = this.createStore.get('created_by').value;
+    data.store_adress = this.createStore.get('store_adress').value;
+
+    console.log(data);
+  }
 }
