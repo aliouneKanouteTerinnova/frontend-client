d5c2b3b8a169b399b9ccaeccf7ee05399570ba63
Abdoulaye NDAO <abdoulaye.ndao@xegit.com>
2021-06-04 13:27:46 +0000

    fix:Change images on th landing page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,21 +10,6 @@ import { AuthenticationsService } from 'src/app/services/authentications.service
 export class HomeComponent implements OnInit {
   products = [
     {
-      src: 'https://sn.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/50/34406/1.jpg?9854',
-      name: 'TOPICREM Ultra-Hydratant Gel 1L + Rasoir Gillette',
-      price: '20 000 FCFA',
-    },
-    {
-      src: 'https://sn.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/17/58424/1.jpg?6353',
-      name: 'Generic Tensiomètre LCD numérique pour tensiomètre-blanc',
-      price: '30 442 FCFA',
-    },
-    {
-      src: 'https://sn.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/50/34406/1.jpg?9854',
-      name: 'TOPICREM Ultra-Hydratant Gel 1L + Rasoir Gillette',
-      price: '20 000 FCFA',
-    },
-    {
       src: 'https://sn.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/17/58424/1.jpg?6353',
       name: 'Generic Tensiomètre LCD numérique pour tensiomètre-blanc',
       price: '30 442 FCFA',
