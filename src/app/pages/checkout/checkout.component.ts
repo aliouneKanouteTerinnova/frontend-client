91245d2b2aea8c5db48716347deaebfd912fd612
Abdoulaye NDAO <abdoulaye.ndao@xegit.com>
2021-06-11 10:16:16 +0000

    feat:checkout getting data from form

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -6,7 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent implements OnInit {
-  constructor() {}
+  checkoutForm: FormGroup;
+  emailRegex = /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
 
-  ngOnInit(): void {}
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.checkoutForm = this.formBuilder.group({
+      firstname: [null, Validators.required],
+      email: [null, [Validators.required, Validators.pattern(this.emailRegex)]],
+      address: [null, Validators.required],
+      city: [null, Validators.required],
+      state: [null, Validators.required],
+      zip: [null, Validators.required],
+      cardname: [null, Validators.required],
+      cardnumber: [null, Validators.required],
+      expmonth: [false, Validators.required],
+      expyear: [null, Validators.required],
+      cvv: [null, Validators.required],
+      sameadr: [false, Validators.required],
+    });
+  }
+  register() {
+    console.log(this.checkoutForm.value);
+  }
 }
