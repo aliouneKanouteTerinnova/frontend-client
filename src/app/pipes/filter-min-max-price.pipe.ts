240615371474b25a4a0da2aab5618ecd024cf028
Abdoulaye NDAO <abdoulaye.ndao@xegit.com>
2021-07-16 13:35:48 +0000

    feat:filtering price by min and max v3

--- src/app/pipes/filter-min-max-price.pipe.ts
+++ src/app/pipes/filter-min-max-price.pipe.ts
@@ -7,6 +7,7 @@ export class FilterMinMaxPricePipe implements PipeTransform {
   transform(value: any, args?: any): any {
     if (!value) return null;
     if (args.length === 0) return value;
+    if (!args[0] && !args[1]) return value;
 
     return value.filter(function (item) {
       return Number(args[1]) >= Number(item.price) && Number(item.price) > Number(args[0]);
