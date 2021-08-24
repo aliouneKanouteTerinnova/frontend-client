6f3232b1a81416e816024853fa8671824339771e
Mbaye Jacques DIOP <mbaye.diop@xegit.com>
2021-08-24 16:09:18 +0000

    feat:implement of  navbar nav

--- src/app/components/buyer/buyer-navbar/buyer-navbar.component.ts
+++ src/app/components/buyer/buyer-navbar/buyer-navbar.component.ts
@@ -1,3 +1,9 @@
+import { AuthResponded } from './../../../models/auth/auth';
+import { CartService } from './../../../services/cart/cart.service';
+import { CartModelServer } from './../../../models/cart/cart';
+import { Router } from '@angular/router';
+import { CategoriesService } from 'src/app/services/categories/categories.service';
+import { AuthenticationsService } from 'src/app/services/authentications/authentications.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +12,88 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buyer-navbar.component.scss'],
 })
 export class BuyerNavbarComponent implements OnInit {
-  constructor() {}
+  category = [];
+  subCategory: string;
+  categoryProdact: [] = [];
+  imageSource: string;
+  showImage = false;
+  cartData: CartModelServer;
+  totalProduct: Number;
+  total = 0;
+  cartTotal: Number;
+  isSeller = false;
+  currentUser: AuthResponded;
+  countries = [{ icon: '/assets/img/lang/United_Kingdom.svg' }, { icon: '/assets/img/lang/United_Kingdom.svg' }];
+  constructor(
+    private authService: AuthenticationsService,
+    private categoriesService: CategoriesService,
+    private router: Router,
+    public cartService: CartService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.currentUser = this.authService.currentUserValue;
+    if (this.currentUser != null) {
+      this.authService.getUser(this.currentUser['user'].token).subscribe((data) => {
+        // this.user = data.body;
+
+        if (this.currentUser['user'].account_type === 'SELLER' || this.currentUser['user'].account_type === 'Seller') {
+          this.isSeller = true;
+        }
+      });
+    }
+
+    this.cartService.cartDataObs$.subscribe((data: CartModelServer) => {
+      this.cartData = data;
+      for (let inded of this.cartData.data) {
+        this.total += inded.numInCart;
+      }
+    });
+
+    this.cartService.cartTotal$.subscribe((total) => {
+      this.cartTotal = total;
+    });
+
+    this.cartService.productTotal$.subscribe((totalProduct) => {
+      this.totalProduct = totalProduct;
+    });
+
+    this.getCategory();
+  }
+
+  getCategory(): void {
+    this.categoriesService.getAllCategories().subscribe((res) => {
+      console.log(res.results);
+      console.log(res.results[0].children);
+      this.category = res.results;
+    });
+  }
+
+  showCategory(data, id: number): void {
+    this.subCategory = this.category[id].children;
+    console.log(this.subCategory);
+    this.imageSource = data.image;
+    this.showImage = true;
+  }
+
+  changeImageCategory(data): void {
+    this.imageSource = data.image;
+  }
+
+  oNCategoryDetails(data): void {
+    console.log(data.id);
+  }
+
+  searchProducts(keyWord: string) {
+    if (keyWord) {
+      this.router.navigate([`product/${keyWord}`]);
+    } else {
+      return;
+    }
+  }
+
+  logout(): void {
+    this.authService.logOut();
+    window.location.reload();
+  }
 }
