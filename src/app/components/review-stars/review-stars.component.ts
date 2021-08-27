73a2ff11a9f5c04516a89d165ad94b3c2aff41f3
Mouhamed Bakhoum <mouhamed.bakhoum@terinnova.com>
2021-08-27 12:50:14 +0000

    feat:completed review stars component

--- src/app/components/review-stars/review-stars.component.ts
+++ src/app/components/review-stars/review-stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-review-stars',
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./review-stars.component.css'],
 })
 export class ReviewStarsComponent implements OnInit {
+  @Input() rating: any;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  getRatingArray(rating: any) {
+    return [...Array(5 - Math.floor(Number(rating))).keys()];
+  }
+
+  getCheckedRatingArray(rating: any) {
+    return [...Array(Math.floor(Number(rating))).keys()];
+  }
+  parseRating(rating: any) {
+    return Math.floor(rating);
+  }
 }
