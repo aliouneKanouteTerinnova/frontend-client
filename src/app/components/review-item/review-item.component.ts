1d1e13b6f967dfca646b9f8fde274563d3c24391
Mouhamed Bakhoum <mouhamed.bakhoum@terinnova.com>
2021-08-27 12:49:48 +0000

    feat:completed review item component

--- src/app/components/review-item/review-item.component.ts
+++ src/app/components/review-item/review-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-review-item',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./review-item.component.scss'],
 })
 export class ReviewItemComponent implements OnInit {
+  @Input() review: any;
+
   constructor() {}
 
   ngOnInit(): void {}
