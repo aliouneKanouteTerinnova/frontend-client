fba99f384ec519085ca42c99f59b11d9bacf540d
Mouhamed Bakhoum <mouhamed.bakhoum@terinnova.com>
2021-08-31 13:37:02 +0000

    feat:completed order status component

--- src/app/components/order-status/order-status.component.ts
+++ src/app/components/order-status/order-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-order-status',
@@ -6,7 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-status.component.scss'],
 })
 export class OrderStatusComponent implements OnInit {
+  @Input() status;
+  progressValue: number;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    switch (this.status) {
+      case 'canceled':
+        this.progressValue = 0;
+      case 'initiated':
+        this.progressValue = 25;
+        break;
+      case 'confirmed':
+        this.progressValue = 50;
+        break;
+      case 'shipping':
+        this.progressValue = 75;
+        break;
+      case 'delivered':
+        this.progressValue = 100;
+        break;
+      default:
+        break;
+    }
+  }
 }
