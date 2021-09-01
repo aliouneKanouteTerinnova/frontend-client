d303d94cce0c058ea801e0bc57c37b5121929aef
Mouhamed Bakhoum <mouhamed.bakhoum@terinnova.com>
2021-09-01 17:05:11 +0000

    test: write unit test

--- src/app/components/review-item/review-item.component.spec.ts
+++ src/app/components/review-item/review-item.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { first } from 'rxjs/operators';
 
 import { ReviewItemComponent } from './review-item.component';
 
@@ -20,5 +21,27 @@ describe('ReviewItemComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get a review as prop', () => {
+    expect(component.review).toBeTruthy();
+  });
+
+  it('should get the current user', () => {
+    expect(component.currentUser).toBeTruthy();
+  });
+
+  it('should emit an update review event', () => {
+    component.btnUpdateReviewClick.pipe(first()).subscribe((value: string) => {
+      expect(value).toBe('update review');
+    });
+    component.onReviewUpdate();
+  });
+
+  it('should emit an delete review event', () => {
+    component.btnDeleteReviewClick.pipe(first()).subscribe((value: string) => {
+      expect(value).toBe('delete review');
+    });
+    component.onReviewDelete();
   });
 });
