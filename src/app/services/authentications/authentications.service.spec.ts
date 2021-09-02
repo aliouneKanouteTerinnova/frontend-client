c13d27c3252e02e1c17457c25be7d9c0f238c0dc
Mouhamed Bakhoum <mouhamed.bakhoum@terinnova.com>
2021-09-02 18:15:15 +0000

    feat: mock class auth service for unit test

--- src/app/services/authentications/authentications.service.spec.ts
+++ src/app/services/authentications/authentications.service.spec.ts
@@ -1,9 +1,18 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { AccountType } from 'src/app/enums/account-type.enum';
+import { Gender } from 'src/app/enums/gender.enum';
 
 import { AuthenticationsService } from './authentications.service';
 
 describe('AuthenticationsService', () => {
   let service: AuthenticationsService;
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -14,3 +23,22 @@ describe('AuthenticationsService', () => {
     expect(service).toBeTruthy();
   });
 });
+
+export class MockAuthService {
+  currentUserValue = {
+    user: {
+      id: '',
+      email: '',
+      username: 'Mouhamed',
+      token: 'abcdef',
+      account_type: AccountType.Customer,
+      gender: Gender.M,
+      address: {
+        state: '',
+        zipcode: '',
+        country: '',
+        street: '',
+      },
+    },
+  };
+}
