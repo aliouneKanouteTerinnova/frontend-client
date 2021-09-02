ada8eda1615adf7de1002e4488be9371217786d8
Fatma Gaye LO <FGLO>
2021-09-02 11:11:58 +0000

    change backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://3.126.109.165:8080/api/',
-
+  baseUrl: 'http://3.126.109.165:8081/api/',
 };
 /*
  * For easier debugging in development mode, you can import the following file
