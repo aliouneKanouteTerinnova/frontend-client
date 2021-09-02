bbbb309a344f396318f374c2ab981ed81d73d5b9
Mouhamed Bakhoum <mouhamed.bakhoum@terinnova.com>
2021-09-02 18:18:07 +0000

    feat:write unit test for review stars  component

--- src/app/components/review-stars/review-stars.component.spec.ts
+++ src/app/components/review-stars/review-stars.component.spec.ts
@@ -15,10 +15,24 @@ describe('ReviewStarsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ReviewStarsComponent);
     component = fixture.componentInstance;
+    // avoid errors in the template
+    component.rating = 4;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should parse the rating', () => {
+    expect(component.parseRating(4)).toBe(4);
+  });
+
+  it('get the yellow stars number', () => {
+    expect(component.getCheckedRatingArray(4).length).toBe(4);
+  });
+
+  it('get the black stars number', () => {
+    expect(component.getRatingArray(4).length).toBe(1);
   });
 });
