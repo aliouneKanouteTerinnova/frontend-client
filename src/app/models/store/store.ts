8b8ecb75a9e0574fb03bc1717d1eea808eaeb8a3
Fatma Gaye LO <FGLO>
2021-09-28 11:58:29 +0000

    add address field to store

--- src/app/models/store/store.ts
+++ src/app/models/store/store.ts
@@ -1,8 +1,11 @@
+import { Address } from '../address/address';
+
 export class Store {
   name: string;
   // created_at: string;
   created_by: number;
-  store_address: string;
+  // store_address: string;
+  address: Address;
   is_active: boolean;
   products: any;
   image: string;
