3ef0faf9d7a8954c3d471b4bb6992b3a4ac351c4
Alioune Kanoute <alioune.kanoute@xegit.com>
2021-10-01 12:53:37 +0000

    feature: shipment service

--- src/app/services/shipments/shipment.service.ts
+++ src/app/services/shipments/shipment.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShipmentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  update(token: any, id: string, data: any){
+    token = 'token ' + token;
+    return this.http.put<any>(`${environment.baseUrl}/shippings/shipments/${id}`, data, {
+      headers: new HttpHeaders().set('Authorization', token),
+      observe: 'response',
+    });
+  }
+
 }
