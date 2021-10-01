467db63677a26552bfb461cf08d654098902a87a
Fatma Gaye LO <FGLO>
2021-10-01 09:46:14 +0000

    before merge last stable version

--- src/app/pages/products/product-detail/product-detail.component.ts
+++ src/app/pages/products/product-detail/product-detail.component.ts
@@ -29,6 +29,7 @@ export class ProductDetailComponent implements OnInit {
   similarProducts = [];
   currentUser: any;
   isOwner: any;
+  hasOrdered = false;
   constructor(
     private productsService: ProductsService,
     private router: ActivatedRoute,
